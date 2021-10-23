9c8c17a7e62dea8549b6d96cb88bc0eda8fd2d66
Eric Mena <53233372+EricM76@users.noreply.github.com>
2021-10-23 11:57:46 -0300

    add readme & validations register

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,3 +1,165 @@
-window.addEventListener('load',() => {
-    console.log('register.js connected success')
+let regExLetter = /^[A-Z]+$/i;
+let regExEmail =  /^(([^<>()\[\]\.,;:\s@\”]+(\.[^<>()\[\]\.,;:\s@\”]:+)*)|(\”.+\”))@(([^<>()[\]\.,;:\s@\”]+\.)+[^<>()[\]\.,;:\s@\”]{2,})$/;
+let regExPass = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,12}$/; //mayuscula, numero y 6 a 12 caracteres
+let regExPass2 = /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/; //mayuscula, numero, especial y 8 a 16 caracteres
+
+const emailVerify = async (email) => {
+    try {
+        let response = await fetch('http://localhost:3000/api/emails');
+        let result = await response.json()
+
+        return result.data.includes(email)
+               
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+window.addEventListener('load', () => {
+    console.log('resgister.js success')
+
+    $('name').addEventListener('focus', () => {
+        
+        $('name').classList.remove('is-invalid')
+
+
+    })
+    $('name').addEventListener('blur', () => {
+
+        switch (true) {
+            case !$('name').value.trim():
+                $('name-error').innerHTML = "<span><i class='fas fa-exclamation-triangle'></i> El nombre es obligatorio</span>"
+                $('name').classList.add('is-invalid')
+
+                break;
+            case $('name').value.trim().length < 2 || $('name').value.trim().length > 50 :
+                $('name-error').innerText = "Entre 2 y 50 caracteres"
+                $('name').classList.add('is-invalid')
+
+                break;
+            case !regExLetter.test($('name').value.trim()):
+                $('name-error').innerText = "Solo caracteres alfabéticos"
+                $('name').classList.add('is-invalid')
+
+                break;
+            default:
+                $('name').classList.remove('is-invalid')
+                $('name').classList.add('is-valid')
+                $('name-error').innerText = null
+                break;
+        }
+    })
+    $('name').addEventListener('keydown', () => {
+        $('name').classList.remove('is-invalid')
+        $('name-error').innerText = null
+        })
+
+
+        $('email').addEventListener('focus',()=> {
+            $('email').classList.remove('is-invalid')
+        })
+    $('email').addEventListener('blur', async () => {
+
+        switch (true) {
+            case !regExEmail.test($('email').value):
+                $('email-error').innerText = "Tiene que ser un email válido"
+                $('email').classList.add('is-invalid')
+                break;
+            case await emailVerify($('email').value) :
+                $('email-error').innerText = "El email está registrado"
+                $('email').classList.add('is-invalid')
+                break;
+            default:
+                $('email-error').innerText = null
+                $('email').classList.remove('is-invalid')
+                $('email').classList.add('is-valid')
+                break;
+        }
+    })
+
+    $('password').addEventListener('blur',() => {
+        if(!regExPass2.test($('password').value)){
+            $('password-error').innerText = "Una mayúscula, un número, símbolo, 8 a 12 carateres"
+            $('password').classList.add('is-invalid')
+        }else{
+            $('password-error').innerText = null
+            $('password').classList.remove('is-invalid')
+            $('password').classList.add('is-valid')
+        }
+    })
+    $('password').addEventListener('focus',()=> {
+        $('password').classList.remove('is-invalid')
+
+    })
+
+    $('password2').addEventListener('blur',() => {
+        if($('password').value !== $('password2').value){
+            $('password2-error').innerText = "Las contraseñas no coinciden"
+            $('password2').classList.add('is-invalid')
+        }else{
+            if($('password').value !== ""){
+            $('password2-error').innerText = null
+            $('password2').classList.remove('is-invalid')
+            $('password2').classList.add('is-valid')
+            }else{
+                $('password2-error').innerText = null
+
+            }
+        }
+    })
+
+    $('password2').addEventListener('focus',()=> {
+        $('password2').classList.remove('is-invalid')
+    })
+
+    $('terminos').addEventListener('click', () => {
+        $('terminos').classList.toggle('is-valid');
+        $('terminos').classList.toggle('is-invalid');
+        $('terminos-error').innerHTML = null
+
+    })
+
+    $('form-register').addEventListener('submit', event => {
+        event.preventDefault();
+
+        let elementsForm = $('form-register').elements;
+        //console.log(elementsForm);
+        let error = false;
+
+        for (let i = 0; i < elementsForm.length - 2; i++) {
+            
+            if(!elementsForm[i].value){
+                elementsForm[i].classList.add('is-invalid')
+                $('error-empty').innerHTML = "Los campos señalados son obligatorios";
+                error = true
+            }
+        }
+
+        if(!$('terminos').checked) {
+            
+            $('terminos').classList.add('is-invalid')
+            $('terminos-error').innerText = "Debes aceptar los términos y condiciones";
+            error = true
+        }
+
+        for (let i = 0; i < elementsForm.length - 2; i++) {
+            
+            if(elementsForm[i].classList.contains('is-invalid')){
+                error = true
+                /* $('terminos').classList.remove('is-valid') */
+
+            }
+        }
+
+        if(!error){
+            $('form-register').submit()
+        }
+    })
+
+
+    
+
 })
--- readme.md
+++ readme.md
@@ -1,3 +1,25 @@
 # Craftsy 3.0 (2021)
-## Carrito
-### Vista
+Sitio de referencia creado durante la cursada de las comisiones 6, 9 y 10 del curso de Full Stack Developer de la Fundación Formar
+## Instalación y puesta en marcha
+- Luego de CLONAR el proyecto con `git clone https://github.com/EricM76/crafsty3.0.git` entrar a la carpeta __craftsy3.0__
+- Instalar las dependencias con `npm install`
+    - Las siguientes dependencias serán instaladas solo en desarrollo:
+        - (The Sequelize Command Line Interface (CLI))[https://www.npmjs.com/package/sequelize-cli]
+        - (Nodemon)[https://www.npmjs.com/package/nodemon]
+        - (Node-sass)[https://www.npmjs.com/package/node-sass]
+- Abrir el proyecto con el editor de preferencia, como por ejemplo (VSCode)[https://code.visualstudio.com/], y crear en la carpeta raíz un archivo llamado __.env__
+- En archivo __.env__ crear las siguientes variables de entorno:
+~~~
+DB_USERNAME=
+DB_PASSWORD= 
+DB_DATABASE=craftsy_db
+DB_HOST=127.0.0.1
+DB_PORT=3306
+~~~
+*Como valor de __DB_USERNAME__ y __DB_PASSWORD__ escribir las credenciales de su sistema de gestion de base de datos*
+- Crear la base datos con el comando: `sequelize db:create`
+- Correr las migraciones con el comando : `sequelize db:migrate`
+- Correr los seeders con el comando : `sequelize db:seed:all`
+- Levantar el servidor de desarrollo con el comando : `npm run dev`
+### Carrito
+##### Vista
